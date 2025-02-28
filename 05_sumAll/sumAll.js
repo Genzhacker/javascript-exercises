8fb263b569971fb6956c0daf0405ec0990ad4ef4
ODahmani <ODahmani+USERNAME@users.noreply.github.com>
2025-02-28 18:09:09 +0100

    Add the right function content to pass all the tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(x, y) {
+    if (isNaN(x) || isNaN(y) || !Number.isInteger(x) || !Number.isInteger(y) || x < 0 || y < 0) {
+        return "ERROR";
+    }
 
+    const start = Math.min(x, y);
+    const end = Math.max(x, y);
+    let sum = 0;
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
