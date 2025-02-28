2ba1c97d7cdf06cd0ea547fec44730fd7e7bf877
ODahmani <ODahmani+USERNAME@users.noreply.github.com>
2025-02-28 23:01:12 +0100

    Add the right function content to pass all the tests

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degree) {
+  let F = (degree - 32) * 5/9;
+  return Math.round(F * 10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degree) {
+  let C = (degree * 9/5) + 32;
+  return Math.round(C * 10)/10;
 };
 
 // Do not edit below this line
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+  let s = 0;
+	arr.forEach(element => s += element);
+  return s;
 };
 
-const multiply = function() {
-
+const multiply = function(arr) {
+  return arr.reduce((m, element) => m * element, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	if([0, 1].includes(x)){
+    return 1;
+  }else{
+    return x * factorial(x-1);
+  }
 };
 
 // Do not edit below this line
