5d49256690029d4e4ebb4ab96417ede5cb36d050
ODahmani <ODahmani+USERNAME@users.noreply.github.com>
2025-02-28 11:36:17 +0100

    Add the right function content for the tests to pass

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(str, nbr) {
+    let ch = '';
+    if(nbr === 0){
+        return ch;
+    }else if(nbr>0){
+        for(let i = 0; i<nbr; i++){
+            ch+= str;
+        }
+    }else{
+        ch+= "ERROR";
+    }
+    return ch;
 };
 
 // Do not edit below this line
