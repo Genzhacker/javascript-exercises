82ae81d10f381c5d15437bbcf73e1685576c2c50
ODahmani <ODahmani+USERNAME@users.noreply.github.com>
2025-03-01 16:14:51 +0100

    Add the function content so that all the tests pass

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let arr1 = str.toLowerCase().split('').filter(char => (char>='a' && char<='z') || (char>='0' && char<='9'));
+    if(arr1.join('') === arr1.reverse().join('')){
+        return true;
+    }else{
+        return false;
+    }
 };
 
 // Do not edit below this line
