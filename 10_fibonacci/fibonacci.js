6cfa5a4874a851c61e3eaf396eaff4634b9d4f21
ODahmani <ODahmani+USERNAME@users.noreply.github.com>
2025-03-01 17:58:05 +0100

    Add the right function content to pass all the tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(x) {
+    x = parseInt(x);
+    if(x < 0){
+        return "OOPS"
+    }else if(x == 0){
+        return 0;
+    }else if([1, 2].includes(x)){
+        return 1;
+    }else{
+        return fibonacci(x-2) + fibonacci(x-1);
+    }
 };
 
 // Do not edit below this line
