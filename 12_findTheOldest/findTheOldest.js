c3ab68dfe7ec5c722466e7deba2efa67c4da26ba
ODahmani <ODahmani+USERNAME@users.noreply.github.com>
2025-03-02 18:00:53 +0100

    Add the function content so that all the tests pass

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,14 @@
-const findTheOldest = function() {
+function NameOfTheOldest(previous, next){
+    let currentYear = new Date().getFullYear();
+    //let y1 = next.yearOfDeath === undefined? currentYear - next.yearOfBirth : next.yearOfDeath - next.yearOfBirth;
+    //let y2 = previous.yearOfDeath === undefined? currentYear - previous.yearOfBirth : previous.yearOfDeath - previous.yearOfBirth;
+    let y1 = (next.yearOfDeath || currentYear) - next.yearOfBirth;
+    let y2 = (previous.yearOfDeath || currentYear) - previous.yearOfBirth;
+    return y1>y2? next : previous;
+}
 
+const findTheOldest = function(arr) {
+    return arr.reduce(NameOfTheOldest);
 };
 
 // Do not edit below this line
